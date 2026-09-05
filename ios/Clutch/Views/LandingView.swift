a2105a4ad7bbbad7716b99c5193bb3145c01e29e
Navi <postboxva6@gmail.com>
2026-09-05 17:56:36 -0400

    added girl picture + clutch's font for the landing page

--- ios/Clutch/Views/LandingView.swift
+++ ios/Clutch/Views/LandingView.swift
@@ -17,6 +17,20 @@ struct LandingView: View {
                 .ignoresSafeArea()
                 .allowsHitTesting(false)
             }
+            .overlay(alignment: .top) {
+                VStack(spacing: 18) {
+                    Image("girl")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 260)
+                        .shadow(color: .white.opacity(0.55), radius: 34)
+
+                    Text("Clutch")
+                        .font(.custom("PinyonScript-Regular", size: 62))
+                        .foregroundStyle(.white)
+                }
+                .padding(.top, 90)
+            }
             .background(Color.black)
     }
 }
